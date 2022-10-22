7a16c0bb0a280c080b622281c369106a169c4cf7
Olusola <doolatunde@gmail.com>
2022-10-22 15:09:06 +0000

    Refac2

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,6 +6,6 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-2qw8fdpq.us.auth0.com',            // Auth0 domain
-  clientId: 'Yp8sIAfKUvnscdmeGsTkJzWzN8HBOJRO',          // Auth0 client id
+  clientId: 'rFKCiOeVX7Nk2tzo0Ez9a4Y7fn5pgnIk',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
